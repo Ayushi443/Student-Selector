956213853774226089397795599ce9ddd0f30682
Ayushi Rathi <134195455+Ayushi443@users.noreply.github.com>
2023-10-26 00:30:21 +0530

    Update dc.py
    Updated code

--- dc.py
+++ dc.py
@@ -1,17 +1,22 @@
 import pandas as pd
-
+import re
 # Step 3: Filter and Merge Data
-def process_data(file_path, cfti_colleges):
-    # Read the Excel file with three sheets
+def process_data(file_path, cfti_colleges):  
+    
     xls = pd.ExcelFile(file_path)
 
     # Create an empty DataFrame to store the filtered data
     filtered_data = pd.DataFrame(columns=['Name', 'University', 'Graduation_Marks', 'CFTI YES/NO'])
 
-    # Loop through each sheet and fikmlncoienlter stumnb,dents
+    # Loop through each sheet and filter students
     for sheet_name in xls.sheet_names:
         sheet_data = pd.read_excel(xls, sheet_name)
-        filtered_data = pd.concat([filtered_data, sheet_data[sheet_data['Graduation Marks'] > 8]])
+        
+        # Check if "Graduation Marks" (CG) are greater than 10 and meet the additional conditions
+        sheet_data = sheet_data[(sheet_data['Graduation Marks'] > 10) & (sheet_data['Graduation Marks'] > 80) |
+                                (sheet_data['Graduation Marks'] <= 10) & (sheet_data['Graduation Marks'] >= 8)]
+
+        filtered_data = pd.concat([filtered_data, sheet_data])
 
     # Update "CFTI YES/NO" Column
     filtered_data['CFTI YES/NO'] = filtered_data['University'].apply(lambda uni: 'YES' if uni in cfti_colleges else 'NO')
@@ -24,6 +29,7 @@ def save_to_excel(dataframe, output_path):
 # Provided array of CFTI colleges
 provided_cfti_colleges = [
     "INDIAN INSTITUTES OF TECHNOLOGY",
+    "IIT"
     "INDIAN INSTITUTES OF MANAGEMENT ",
     "INDIAN INSTITUTE OF SCIENCE (IISc), BANGALORE",
     "INDIAN INSTITUTES OF SCIENCE EDUCATION AND RESEARCH (IISERs) ",
@@ -56,4 +62,28 @@ result = process_data(file_path, provided_cfti_colleges)
 # Save the result to a new Excel file
 save_to_excel(result, output_excel_path)
 
-print("Filtered data saved to:", output_excel_path)
+#print("Filtered data saved to:", output_excel_path)
+
+output_excel_sheet = pd.read_excel(output_excel_path)
+
+# Create an array of graduation universities from the output Excel data
+graduation_universities_list = output_excel_sheet['Graduation University'].unique()
+
+# Define a regex pattern to match universities with specific keywords
+keywords_pattern = r"(?i)(INDIAN\s*INSTITUTES?\s*OF\s*TECHNOLOGY|IIT|INDIAN\s*INSTITUTES?\s*OF\s*MANAGEMENT|IIM|INDIAN\s*INSTITUTE\s*OF\s*SCIENCE.BANGALORE|INDIAN\s*INSTITUTES?\s*OF\s*SCIENCE.*RESEARCH|IISER|Indian\s*Institute\s*of\s*Information\s*Technology\s*Allahabad|Atal\s*Bihari\s*Vajpayee\s*Indian\s*Institute\s*of\s*Information\s*Technology.*Gwalior|ABVIIITM|Pandit\s*Dwarka\s*Prasad\s*Mishra\s*Indian\s*Institute\s*of\s*Information\s*Technology.*Jabalpur|IIITDM|Indian\s*Institute\s*of\s*Information\s*Technology.*Kanchipuram|IITD&M|Indian\s*Institute\s*of\s*Information\s*Tehnology.*Kurnool.*Andhra\s*Pradesh|IIITDM|NATIONAL\s*INSTITUTES?\s*OF\s*TECHNICAL\s*TEACHERS?\s*TRAINING.*RESEARCH|NITTTR|NATIONAL\s*INSTITUTES?\s*OF\s*TECHNOLOGY|NIT|National\s*Institute\s*of\s*Industrial\s*Engg.*Mumbai|NITIE|National\s*Institute\s*of\s*Foundry\s&\s*Forge\s*Technology.Ranchi|NIFFT|School\s*of\s*Planning\s&\s*Architecture.Delhi|School\s*of\s*Planning\s&\s*Architecture.Bhopal|School\s*of\s*Planning\s&\s*Architecture.*Vijayawada|Central\s*Institute\s*of\s*Technology.*Kokrajhar|Sant\s*Longowal\s*Institute\s*of\s*Engineering.*Technology|Sant\s*Longowal\s*Institute\s*of\s*Engineering.*Technology.*Punjab|North\s*Eastern\s*Regional\s*Institute\s*of\s*Science.*Technology.*Itanagar|Ghani\s*Khan\s*Choudhury\s*Institute\s*of\s*Engineering.*Technology.*Malda.*West\s*Bengal)"
+
+def is_cfti(university):
+    return bool(re.search(keywords_pattern, university, re.IGNORECASE))
+
+# Load the filtered data from the output Excel file
+output_excel_path = 'C:\\Users\\AYUSHI\\OneDrive\\Desktop\\DC\\Filtered_Result.xlsx'
+output_excel_sheet = pd.read_excel(output_excel_path)
+
+# Filter rows in output_excel_sheet where the university matches the criteria
+output_excel_sheet = output_excel_sheet[output_excel_sheet['Graduation University'].apply(is_cfti)]
+
+# Save the filtered data to a new Excel file
+filtered_output_excel_path = 'C:\\Users\\AYUSHI\\OneDrive\\Desktop\\DC\\Filtered_CFTI_Result.xlsx'
+output_excel_sheet.to_excel(filtered_output_excel_path, index=False)
+
+print("Filtered CFTI data saved to:", filtered_output_excel_path)
